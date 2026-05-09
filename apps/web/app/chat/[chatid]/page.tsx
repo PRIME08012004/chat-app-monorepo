60dc7e3dd0d8983a4298bf9e86df89a11bb1ee4e
Omkar_Shirsekar <iamomkar0007@gmail.com>
2026-05-09 23:34:04 +0530

    init http and ws servers

---
+++ apps/web/app/chat/[chatid]/page.tsx
@@ -0,0 +1,12 @@
+export default function ChatInterface() {
+  return (
+    <>
+      <div className=" min-h-screen flex justify-center items-center w-full">
+        <div className="bg-gray-700 shadow-xl/20 shadow-slate-50 ">
+        
+        
+        </div>
+      </div>
+    </>
+  );
+}
